dcc79cb64ca6906012c44918b7ffc90fbe345a56
Vivvianne Kimani <ndiahav@gmail.com>
2019-07-07 22:33:30 +0300

    creating bash_profile for config

--- manage.py
+++ manage.py
@@ -1,22 +1,6 @@
-from flaskblog import create_app, db
-from flask_script import Manager, Server
-from flaskblog.models import User,Post
+from flaskblog import create_app
 
-from flask_migrate import Migrate, MigrateCommand
-
-from app import app, db
-
-app = create_app('development')
-#app = create_app('production')
-app.config.from_object(os.environ['APP_SETTINGS'])
-
-manager = Manager(app)
-manager.add_command('server', Server)
-
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
